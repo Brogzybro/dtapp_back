ebc71b3c5342e0ee22c89787881ff8a5ca658626
Eivind Fjeldstad <eivind.fjeldstad@gmail.com>
2019-05-28 10:41:20 +0200

    Fix filters

--- controllers/samples.js
+++ controllers/samples.js
@@ -2,20 +2,30 @@ const Sample = require('../models/Sample');
 
 exports.list = async(ctx) => {
   const { user } = ctx.state;
-  const { type, startDate, endDate } = ctx.query;
+  const { type, startDate, endDate, limit, offset } = ctx.query;
 
-  const query = Sample.find({ user: user.id });
+  const query = Sample
+    .find({ user: user.id })
+    .sort('-startDate');
 
   if (type) {
     query.where('type').eq(type);
   }
 
   if (startDate) {
-    query.where('startDate').gte(startDate);
+    query.where('startDate').gte(parseInt(startDate));
   }
 
   if (endDate) {
-    query.where('endDate').lte(endDate);
+    query.where('endDate').lte(parseInt(endDate));
+  }
+
+  if (limit) {
+    query.limit(parseInt(limit));
+  }
+
+  if (offset) {
+    query.skip(parseInt(offset));
   }
 
   ctx.body = await query;
