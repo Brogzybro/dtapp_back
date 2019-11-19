c3bfcf9e385ba838fab167c505f6029cca4bd971
Asd <Asd>
2019-11-19 04:44:12 +0100

    hmm

--- jobs/fitbit.test.js
+++ jobs/fitbit.test.js
@@ -2,6 +2,7 @@ const mongoTestConfig = require('../config').mongo_test;
 require('../db').init(mongoTestConfig);
 const fitbitSync = require('../jobs/fitbit');
 
-it('should sync fitbit without errors', done => {
-  fitbitSync(done);
+it('should sync fitbit without errors', async () => {
+  await fitbitSync();
+  //
 });
