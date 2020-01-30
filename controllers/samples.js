3304c15ac263b4240dd18bddbc076f333b469a5c
Asd <Asd>
2020-01-30 06:14:00 +0100

    sample hard limit and logging

--- controllers/samples.js
+++ controllers/samples.js
@@ -1,12 +1,17 @@
 const Sample = require('../models/Sample');
 
-exports.list = async(ctx) => {
+exports.list = async ctx => {
   const { user } = ctx.state;
   const { type, startDate, endDate, limit, offset } = ctx.query;
 
-  const query = Sample
-    .find({ user: user.id })
-    .sort('-startDate');
+  const query = Sample.find({ user: user.id }).sort('-startDate');
+
+  console.log('Sample request from user ' + user);
+  console.log('type: ' + type);
+  console.log('startDate: ' + startDate);
+  console.log('endDate: ' + endDate);
+  console.log('limit: ' + limit);
+  console.log('offset: ' + offset);
 
   if (type) {
     query.where('type').eq(type);
@@ -20,13 +25,18 @@ exports.list = async(ctx) => {
     query.where('endDate').lte(parseInt(endDate));
   }
 
-  if (limit) {
-    query.limit(parseInt(limit));
-  }
+  // Hard limit to not run out of memory
+  // (~80k-85k records causes issues, 256 MB limit on Heroku)
+  var nlimit = 20000;
+  if (limit < nlimit) nlimit = limit;
+  if (limit < 1) nlimit = 1;
+  query.limit(parseInt(nlimit));
 
   if (offset) {
     query.skip(parseInt(offset));
   }
 
-  ctx.body = await query;
+  const ret = await query;
+  console.log(ret.length);
+  ctx.body = ret;
 };
