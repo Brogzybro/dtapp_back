14ae81bc51488bd7d27734cdc702a8d75a552c26
Asd <Asd>
2020-02-09 23:50:49 +0100

    systolic pressure sync

--- jobs/withings.js
+++ jobs/withings.js
@@ -44,78 +44,116 @@ async function sync() {
   for await (const token of tokens) {
     const { data, user: userId } = token;
     const { access_token: accessToken, refresh_token: refreshToken } = data;
-    console.log(
-      'acessToken: ' + accessToken + '\ndata: ' + data + '\nuserid: ' + userId
-    );
 
-    const latest = await Sample.findLatest({
-      user: userId,
-      type: 'diastolicBloodPressure',
-      source: 'withings'
+    /*
+    await Sample.deleteMany({
+      type: { $in: ['diastolicBloodPressure', 'systolicBloodPressure'] }
     });
-    console.log(latest);
-    var latestTime = null;
-    if (latest) {
-      latestTime = latest.startDate.getTime() / 1000 + 1;
-      console.log(latestTime);
-    }
+    return;
+    */
 
-    try {
-      // request.parse['text/json'] = text => JSON.parse(text);
-      const queries = {
-        action: 'getmeas',
-        meastype: 9
-      };
-      if (latestTime) {
-        queries.lastupdate = latestTime;
-      }
-
-      const res = await request
-        .get(measureUrl)
-        .auth(accessToken, { type: 'bearer' })
-        .query(queries);
-
-      // Parsing is broken for some reason, manually parse instead
-      const { status, body } = JSON.parse(res.text);
-      console.log(res.text);
-      console.log(status);
-
-      if (status === 401) {
-        refreshUserToken(userId, refreshToken);
-        return sync();
-      }
-
-      // console.log(body);
-      // console.log(body.measuregrps.length);
-      const measures = body.measuregrps.map(grp => {
-        return {
-          date: new Date(grp.date * 1000),
-          value: grp.measures[0].value
-        };
-      });
-
-      measures.forEach(measure => {
-        samples.push(
-          new Sample({
-            user: userId,
-            type: 'diastolicBloodPressure',
-            value: measure.value,
-            startDate: measure.date,
-            endDate: measure.date,
-            source: 'withings'
-          })
-        );
-      });
-
-      // console.log(measures);
-    } catch (error) {
-      console.log('withings job sync err:' + error);
-    }
+    samples.push(
+      ...(await syncMeasure(
+        userId,
+        'diastolicBloodPressure',
+        accessToken,
+        refreshToken,
+        measureUrl
+      ))
+    );
+    samples.push(
+      ...(await syncMeasure(
+        userId,
+        'systolicBloodPressure',
+        accessToken,
+        refreshToken,
+        measureUrl
+      ))
+    );
   }
 
   // console.log(tokens);
   console.log(samples);
   await Sample.insertMany(samples);
+}
+
+const meastypeMap = {
+  diastolicBloodPressure: 9,
+  systolicBloodPressure: 10
+};
+
+async function syncMeasure(
+  userId,
+  type,
+  accessToken,
+  refreshToken,
+  measureUrl
+) {
+  const samples = [];
+  const latest = await Sample.findLatest({
+    user: userId,
+    type: type,
+    source: 'withings'
+  });
+  console.log(latest);
+  var latestTime = null;
+  if (latest) {
+    latestTime = latest.startDate.getTime() / 1000 + 1;
+    console.log(latestTime);
+  }
+
+  try {
+    // request.parse['text/json'] = text => JSON.parse(text);
+    const queries = {
+      action: 'getmeas',
+      meastype: meastypeMap[type]
+    };
+    if (latestTime) {
+      queries.lastupdate = latestTime;
+    }
+
+    const res = await request
+      .get(measureUrl)
+      .auth(accessToken, { type: 'bearer' })
+      .query(queries);
+
+    // Parsing is broken for some reason, manually parse instead
+    const { status, body } = JSON.parse(res.text);
+    console.log(res.text);
+    console.log(status);
+
+    if (status === 401) {
+      refreshUserToken(userId, refreshToken);
+      return sync();
+    }
+
+    // console.log(body);
+    // console.log(body.measuregrps.length);
+    const measures = body.measuregrps.map(grp => {
+      return {
+        date: new Date(grp.date * 1000),
+        value: grp.measures[0].value
+      };
+    });
+
+    measures.forEach(measure => {
+      samples.push(
+        new Sample({
+          user: userId,
+          type: type,
+          value: measure.value,
+          startDate: measure.date,
+          endDate: measure.date,
+          source: 'withings'
+        })
+      );
+    });
+    return samples;
+    // console.log(measures);
+  } catch (error) {
+    console.log('withings job sync err:' + error);
+    return [];
+  }
 }
 
 module.exports = sync;
