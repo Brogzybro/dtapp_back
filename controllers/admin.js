073f22664bddf410f9317912a52f03f52f0583fc
Asd <Asd>
2020-02-23 21:11:08 +0100

    admin command panel

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,28 +1,43 @@
+/* eslint-disable no-multi-str */
 const Sample = require('../models/Sample');
+
+const commands = {
+  fitbitsync: () => require('../jobs/fitbit')(),
+  withingssync: () => require('../jobs/withings')(),
+  withingswipebp: async () => {
+    await Sample.deleteMany({
+      type: { $in: ['systolicBloodPressure', 'diastolicBloodPressure'] }
+    });
+  },
+  withingswipeheart: async () => {
+    await Sample.deleteMany({
+      type: 'ecg'
+    });
+  },
+  test: async () => {
+    console.log('test admin command');
+  }
+};
 
 exports.adminCommand = async ctx => {
   ctx.status = 200;
   if (ctx.query && ctx.query.action) {
-    switch (ctx.query.action) {
-      case 'fitbitsync':
-        require('../jobs/fitbit')();
-        break;
-      case 'withingssync':
-        require('../jobs/withings')();
-        break;
-      case 'withingswipebp':
-        await Sample.deleteMany({
-          type: { $in: ['systolicBloodPressure', 'diastolicBloodPressure'] }
-        });
-        break;
-      case 'withingswipeheart':
-        await Sample.deleteMany({
-          type: 'ecg'
-        });
-        break;
-      default:
-        console.log('Unrecognized admin command');
-        break;
+    const command = commands[ctx.query.action];
+    if (command) command();
+    else ctx.body = 'Unknown command';
+  } else {
+    let scriptStr = '';
+    let htmlStr = '';
+    for (let cmd in commands) {
+      scriptStr += 'function myFunc' + cmd + "(){fetch('?action=" + cmd + "')}";
+      htmlStr +=
+        '<button onclick="myFunc' +
+        cmd +
+        '()">Run ' +
+        cmd +
+        '</button><br><br>';
     }
+
+    ctx.body = '<script>' + scriptStr + '</script><h1>Commands</h1>' + htmlStr;
   }
 };
