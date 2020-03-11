f445a9e8c5b3558b82da641a7e02b0af1cf9299e
Asd <Asd>
2020-03-11 06:50:09 +0100

    shared user test

--- tests/models/shared_user.test.js
+++ tests/models/shared_user.test.js
@@ -1,18 +1,38 @@
 const testlib = require('../_helpers/jobstestlib');
 const SharedUser = require('../../models/shared_user_model');
+const logger = require('../../config').winston.loggers.default;
 
-describe('', () => {
-  beforeEach(() => {
+describe('shared user', () => {
+  beforeEach(async done => {
     // setup memory db and stuff
-    testlib.setup();
+    await testlib.setup();
+    done();
   });
-  afterEach(() => {
+  afterEach(async done => {
     // cleanup db and stuff
-    testlib.after();
+    await testlib.after();
+    done();
   });
-  it('shared user should be empty', async done => {
+  it('should be empty and throws no errors', async done => {
     // do something
     const sharedUsers = await SharedUser.find();
+    expect(sharedUsers.length).toBe(0);
+    done();
+  });
+  it('should create a bunch of sharedusers', async done => {
+    // do something
+    testlib.enableWinstonLogs();
+    try {
+      await SharedUser.create().catch(e => logger.info(e));
+      await SharedUser.create({});
+      await SharedUser.create();
+      await SharedUser.create();
+      await SharedUser.create();
+    } catch (error) {
+      logger.info('trycatch fail %o', error);
+    }
+    const sharedUsers = await SharedUser.find();
+    logger.info('sharedusers: %o', sharedUsers);
     expect(sharedUsers.length).toBe(0);
     done();
   });
